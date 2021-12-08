e38625bff24653003c54889dcba7455a12ecbc1c
Austin Tarver <austinwtarver@gmail.com>
2021-12-08 15:13:53 -0600

    initial setup and CSS

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+
+
+
+
+
+function operate(operator,num1,num2){
+    if(operator==='+'){
+        return add(num1,num2);
+    }else if(operator==='-'){
+        return subtract(num1,num2);
+    }else if(operator==='*'){
+        return multiply(num1,num2);
+    }else{
+        return divide(num1,num2);
+    }
+}
+
+
+
+function add(x,y){
+    return x+y;
+}
+
+function subtract(x,y){
+    return x-y;
+}
+
+function multiply(x,y){
+    return x*y;
+    
+}
+function divide(x,y){
+    return x/y;
+}
