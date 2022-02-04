b61d79a945a6e3eeac3be2cda3cc5312d229d8fa
Austin Tarver <austinwtarver@gmail.com>
2022-02-04 04:33:38 -0600

    added decimal support, fixed bugs

--- script.js
+++ script.js
@@ -5,90 +5,95 @@ dont concat equation string until right before evaluate function is called.
 
 const btns = document.querySelectorAll('button');
 const btn = document.querySelectorAll('.btn');
-const output=document.querySelector('#output');
+const output = document.querySelector('#output');
 const decimal = document.querySelector('#decimal');
 
-answer="";
-equation="";
-operandOne="";
-operandTwo="";
-operator="";
-output.innerText=0;
+answer = "";
+equation = "";
+operandOne = "";
+operandTwo = "";
+operator = "";
+output.innerText = 0;
 btns.forEach((btn) => {
-    btn.addEventListener('click',() => {
+    btn.addEventListener('click', () => {
 
-        if(operator!= "" && btn.id=="operand"){
-            operandTwo+=btn.innerText;
-            output.innerText="";
-            output.innerText=operandTwo;
-        }else if(btn.id=="operand"){
-            operandOne+=btn.innerText;
-            output.innerText=operandOne;
-        }else if(output.innerText==operandOne && btn.id=="decimal"){
-            operandOne+=".";
-            output.innerText=operandOne;
-        }else if(output.innerText==operandTwo && btn.id=="decimal"){
-                operandTwo+=".";
-                output.innerText=operandTwo;    
+        if (operator != "" && btn.id == "operand") {
+            operandTwo += btn.innerText;
+            output.innerText = "";
+            output.innerText = operandTwo;
+        } else if (btn.id == "operand") {
+            operandOne += btn.innerText;
+            output.innerText = operandOne;
+        } else if (output.innerText == operandOne && btn.id == "decimal") {
+            operandOne += ".";
+            output.innerText = operandOne;
+        } else if (output.innerText == operandTwo && btn.id == "decimal") {
+            operandTwo += ".";
+            output.innerText = operandTwo;
         }
 
-        if(operandTwo != "" && answer !="" && btn.id == "operator"){
-            equation=answer + operator + operandTwo;
+        if (operandTwo != "" && answer != "" && btn.id == "operator") {
+            equation = answer + operator + operandTwo;
             console.log(equation);
-            answer=evaluate(equation,operator);
-            operator=btn.innerText;
-            equation=answer;
-            equation+=operator;
-            output.innerText=answer;
-            operandOne="";
-            operandTwo="";
-        }else if(operandTwo != "" && btn.id == "operator"){
-            equation=operandOne + operator + operandTwo;
-            answer=evaluate(equation,operator);
-            operator=btn.innerText;
-            equation=answer;
-            equation+=operator;
-            output.innerText=answer;
-            operandOne="";
-            operandTwo="";     
-        }else if(answer != "" && btn.id == "operator"){
-                operandTwo="";
-                output.innerText=answer;
-                operator=btn.innerText;
-              
-        }else if(btn.id == "operator" ){
-            output.innerText=operandOne;
-             operator=btn.innerText;
+            answer = evaluate(equation, operator);
+            operator = btn.innerText;
+            equation = answer;
+            equation += operator;
+            output.innerText = answer;
+            operandOne = "";
+            operandTwo = "";
+        } else if (operandTwo != "" && btn.id == "operator") {
+            equation = operandOne + operator + operandTwo;
+            answer = evaluate(equation, operator);
+            operator = btn.innerText;
+            equation = answer;
+            equation += operator;
+            output.innerText = answer;
+            operandOne = "";
+            operandTwo = "";
+        } else if (answer != "" && btn.id == "operator") {
+            operandTwo = "";
+            output.innerText = answer;
+            operator = btn.innerText;
+
+        } else if (btn.id == "operator") {
+            output.innerText = operandOne;
+            operator = btn.innerText;
         }
 
-       
-
-        if(answer != "" && btn.id== "equals"){
-            equation=answer+operator+operandTwo;
-            console.log(equation);
-            answer=evaluate(equation,operator);
-            equation=answer;
-            equation+=operator;
-            output.innerText=answer;
-            operandOne="";
-            operandTwo="";
-        }else if(btn.id == "equals"){
-            equation=operandOne + operator + operandTwo;
-            answer=evaluate(equation,operator);  
-            equation=answer;
-            output.innerText=answer;
-            console.log(equation);
-            operandOne="";
-            operandTwo="";
+        if (output.innerText.includes(".")) {
+            document.getElementById("decimal").disabled = true;
+        } else {
+            document.getElementById("decimal").disabled = false;
         }
 
-        if(btn.id == "clear"){
-            answer="";
-            equation="";
-            operator="";
-            operandOne="";
-            operandTwo="";
-            output.innerText=0;
+
+        if (answer != "" && btn.id == "equals") {
+            equation = answer + operator + operandTwo;
+            console.log(equation);
+            answer = evaluate(equation, operator);
+            equation = answer;
+            equation += operator;
+            output.innerText = answer;
+            operandOne = "";
+            operandTwo = "";
+        } else if (btn.id == "equals") {
+            equation = operandOne + operator + operandTwo;
+            answer = evaluate(equation, operator);
+            equation = answer;
+            output.innerText = answer;
+            console.log(equation);
+            operandOne = "";
+            operandTwo = "";
+        }
+
+        if (btn.id == "clear") {
+            answer = "";
+            equation = "";
+            operator = "";
+            operandOne = "";
+            operandTwo = "";
+            output.innerText = 0;
         }
     });
 });
@@ -98,39 +103,48 @@ btns.forEach((btn) => {
 
 
 
-function evaluate(equation, operator){
+function evaluate(equation, operator) {
     console.log(equation);
-    
-    arr=equation.split(operator);
+
+    arr = equation.split(operator);
+    operandOne = arr[0];
+    console.log(operandOne.search(/[.]/g));
+    operandTwo = arr[1];
     console.log(arr);
 
-    if(operator==='+'){
-        return add(parseFloat(arr[0]),parseFloat(arr[1])).toFixed(2);
-    }else if(operator==='-'){
-        return subtract(parseFloat(arr[0]),parseFloat(arr[1])).toFixed(2);
-    }else if(operator==='x'){
-        return multiply(parseFloat(arr[0]),parseFloat(arr[1])).toFixed(2);
-    }else if(operator=="/"){
-        return divide(parseFloat(arr[0]),parseFloat(arr[1])).toFixed(2);
+
+
+
+
+
+    if (operator === '+') {
+        return add(parseFloat(arr[0]), parseFloat(arr[1]));
+    } else if (operator === '-') {
+        return subtract(parseFloat(arr[0]), parseFloat(arr[1]));
+    } else if (operator === 'x') {
+        return multiply(parseFloat(arr[0]), parseFloat(arr[1]));
+    } else if (operator == "/") {
+        return divide(parseFloat(arr[0]), parseFloat(arr[1])).toPrecision(4);
     }
-    
+
 }
 
 
 
 
-function add(x,y){
-    return x+y;
+function add(x, y) {
+    return x + y;
 }
 
-function subtract(x,y){
-    return x-y;
+function subtract(x, y) {
+    return x - y;
 }
 
-function multiply(x,y){
-    return x*y;
-    
+function multiply(x, y) {
+    return x * y;
+
 }
-function divide(x,y){
-    return x/y;
+
+function divide(x, y) {
+    return x / y;
 }
